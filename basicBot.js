468c8c8eeb65fa35b3b6cebd3d355a3a138d9459
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-08-07 12:24:38 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2981,15 +2981,16 @@
                     }
                 }
             },
-                chatonCommand: {
-                command: 'chaton',
+                ocultarCommand: {
+                command: 'ocultar',
                 rank: 'manager',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                            $.getScript('https://rawgit.com/AwayShift/salinhabot/master/chat.js');
+                            $.getScript('https://rawgit.com/AwayShift/salinhabot/master/ocultar.css');
+                            API.sendChat("Oculto!");
                     }
                 }
             },
