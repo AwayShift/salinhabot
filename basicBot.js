e5660a06500abf5587d48ff755822f5579699f23
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-08-09 14:38:14 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -236,7 +236,7 @@
     var botCreatorIDs = ["3851534", "4105209", "6500812"];
 
     var basicBot = {
-        version: "3",
+        version: "1",
         status: false,
         name: "Bot de Teste",
         loggedInID: null,
