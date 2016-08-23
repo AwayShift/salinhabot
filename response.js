9ad2be253a36712229dd88e36a3403eef642919e
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-08-23 17:18:47 -0300

    Update response.js

--- response.js
+++ response.js
@@ -1,18 +1,22 @@
-function theTest(val) {
-  var answer = "";
-  switch( val ) {
-    case 1: case 2: case 3:
-      answer = "Low";
-      break;
-    case 4: case 5: case 6:
-      answer = "Mid";
-      break;
-    case 7: case 8: case 9:
-      answer = "High";
-      break;
-    default:
-      answer = "Massive or Tiny?";
-  } 
-  return answer;  
-  }
-   theTest(9);
+switch (expr) {
+  case "Oranges":
+    console.log("Oranges are $0.59 a pound.");
+    break;
+  case "Apples":
+    console.log("Apples are $0.32 a pound.");
+    break;
+  case "Bananas":
+    console.log("Bananas are $0.48 a pound.");
+    break;
+  case "Cherries":
+    console.log("Cherries are $3.00 a pound.");
+    break;
+  case "Mangoes":
+  case "Papayas":
+    console.log("Mangoes and papayas are $2.79 a pound.");
+    break;
+  default:
+    console.log("Sorry, we are out of " + expr + ".");
+}
+
+console.log("Is there anything else you'd like?");
