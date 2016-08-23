c0e7c5f3569e1fb9807a31981678433c3952471d
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-08-23 16:50:48 -0300

    Update response.js

--- response.js
+++ response.js
@@ -1,11 +1,17 @@
-switch (varName)
-{
-   case "@bot":
-   case "@bot2":
-   case "@bot3": 
-       alert('Hey');
-       break;
-
-   default: 
-       alert('Default case');
-}
+function theTest(val) {
+  var answer = "";
+  switch( val ) {
+    case 1: case 2: case 3:
+      answer = "Low";
+      break;
+    case 4: case 5: case 6:
+      answer = "Mid";
+      break;
+    case 7: case 8: case 9:
+      answer = "High";
+      break;
+    default:
+      answer = "Massive or Tiny?";
+  } 
+  return answer;  
+  }
