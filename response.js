f04e243058ca1935b28501af635e482e5c7fd4c7
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-08-23 17:22:24 -0300

    Update response.js

--- response.js
+++ response.js
@@ -1,22 +1,23 @@
+API.sendChat("Online");
 switch (expr) {
   case "Oranges":
-    console.log("Oranges are $0.59 a pound.");
+    API.sendChat("Oranges are $0.59 a pound.");
     break;
   case "Apples":
-    console.log("Apples are $0.32 a pound.");
+    API.sendChat("Apples are $0.32 a pound.");
     break;
   case "Bananas":
-    console.log("Bananas are $0.48 a pound.");
+    API.sendChat("Bananas are $0.48 a pound.");
     break;
   case "Cherries":
-    console.log("Cherries are $3.00 a pound.");
+    API.sendChat("Cherries are $3.00 a pound.");
     break;
   case "Mangoes":
   case "Papayas":
-    console.log("Mangoes and papayas are $2.79 a pound.");
+    API.sendChat("Mangoes and papayas are $2.79 a pound.");
     break;
   default:
-    console.log("Sorry, we are out of " + expr + ".");
+    API.sendChat("Sorry, we are out of " + expr + ".");
 }
 
 console.log("Is there anything else you'd like?");
