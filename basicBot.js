d7b7b7aefe79cdcf9979c586b22853c2e89d6d8d
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-08-29 22:09:24 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1720,9 +1720,20 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        setInterval(function () {
-                        API.sendChat("Evento teste");
-                        }, 10000);
+                    var delay = setInterval(function () { ("Evento teste"); }, 10000);
+                    API.sendChat(delay)
+                    }
+                }
+            },
+            eventooffCommand: {
+                command: 'eventooff',
+                rank: 'manager',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                    clearTimeout(delay)
                     }
                 }
             },
