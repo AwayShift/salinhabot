f6838b3bd69f93de628fe84954b48b9b66a5f930
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-09-20 12:26:30 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2283,6 +2283,18 @@
                     }
                 }
             },
+                DjCommand: {
+                command: 'dj',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat("/me Para mais informações sobre o evento clique aqui 'DJ'");
+                    }
+                }
+            },
 
             fbCommand: {
                 command: 'fb',
