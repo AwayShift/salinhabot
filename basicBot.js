4078c040540f3b21e02b9b086cee28c588a42d16
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-09-24 09:46:08 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2960,23 +2960,20 @@
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                    API.sendChat(basicBot.chat.pong)
-                   function ping(func, wait) {
-                    var timeout;
-                   return function() {
-                   var context = this, args = arguments;
-                    if (!timeout) {
-                    // the first time the event fires, we setup a timer, which 
-                    // is used as a guard to block subsequent calls; once the 
-                    // timer's handler fires, we reset it and create a new one
-                    timeout = setTimeout(function() {
-                   timeout = 3000;
-                 func.apply(context, args);
-                    }, wait);
-                    }
-                }
-            }
-          }
-        },
+                  (function(ping, undefined){
+                  var canCall = true;
+                  ping.funcName = function(){
+                  if (!canCall) 
+                  return;
+                    //Your function
+                     canCall = false;
+                     setTimeout(function(){
+                      canCall = true;
+                     }, 500);
+                     }
+                   })(window);
+                  }
+                },
             
             refreshCommand: {
                 command: 'refresh',
