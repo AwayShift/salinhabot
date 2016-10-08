fcb599bc7449f85157851c01338bf17546ed7a77
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-10-08 20:19:51 -0300

    Create Grab.js

---
+++ Grab.js
@@ -0,0 +1,9 @@
+API.on(API.GRAB_UPDATE, callback);
+
+function callback(obj) {
+
+    var media = API.getMedia();
+
+    API.sendchat(obj.user.username + " added " + media.author + " - " + media.title);
+
+}
