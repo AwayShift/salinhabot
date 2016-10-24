1f70b555b7f92150356a0f5d90805c30313cf34b
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-10-24 17:22:00 -0200

    Delete Grab.js

--- Grab.js
+++
@@ -1,9 +0,0 @@
-API.on(API.GRAB_UPDATE, callback);
-
-function callback(obj) {
-
-    var media = API.getMedia();
-
-    alert(obj.user.username + " added " + media.author + " - " + media.title);
-
-}
