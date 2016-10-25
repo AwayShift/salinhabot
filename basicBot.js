e98004645cb3b8df8c96bab00a22c8c7a6b184e4
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-10-25 12:51:37 -0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3709,43 +3709,9 @@
                     }
                 }
             }
-    },
+        }
+    };
    
-            brigarCommand: {
-                command: 'brigar',
-                rank: 'user',
-                type: 'startsWith',
-                getBrigar: function (chat) {
-                    var c = Math.floor(Math.random() * basicBot.chat.brigas.length);
-                    return basicBot.chat.brigas[c];
-                },
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        var msg = chat.message;
-
-                        var space = msg.indexOf(' ');
-                        if (space === -1) {
-                            API.sendChat(basicBot.chat.eatbrigar);
-                            return false;
-                        }
-                        else {
-                            var name = msg.substring(space + 2);
-                            var user = basicBot.userUtilities.lookupUserName(name);
-                            if (user === false || !user.inRoom) {
-                                return API.sendChat(subChat(basicBot.chat.nouserbrigar, {name: name}));
-                            }
-                            else if (user.username === chat.un) {
-                                return API.sendChat(subChat(basicBot.chat.selfbrigar, {name: name}));
-                            }
-                            else {
-                                return API.sendChat(subChat(basicBot.chat.brigar, {nameto: user.username, namefrom: chat.un, brigar: this.getBrigar()}));
-                            }
-                        }
-                    }
-                }
-            };
 
     loadChat(basicBot.startup);
     $.getScript('https://rawgit.com/AwayShift/salinhabot/master/entrada.js');
