3c52abb5b7136ede437c12acd54aa743d1ef49df
AwayShift <alessandro_laurindo@yahoo.com.br>
2016-11-02 19:47:15 -0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1240,6 +1240,10 @@
                     API.sendChat(subChat(basicBot.chat.adfly, {name: chat.un}));
                     return true;
                 }
+		if (msg.indexOf('@Awayshift') > -1) {
+                	API.moderateDeleteChat(chat.cid);
+                	API.sendChat('Teste');
+                }
                 if (msg.indexOf('autojoin was not enabled') > 0 || msg.indexOf('AFK message was not enabled') > 0 || msg.indexOf('!afkdisable') > 0 || msg.indexOf('!joindisable') > 0 || msg.indexOf('autojoin disabled') > 0 || msg.indexOf('AFK message disabled') > 0) {
                     API.moderateDeleteChat(chat.cid);
                     return true;
